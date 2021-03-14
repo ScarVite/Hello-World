5f65cfd7807128007e423b578ffe1708b08fc4fd
ScarVite <43887376+ScarVite@users.noreply.github.com>
2021-03-14 13:37:28 +0100

    some refactoring

--- C#/Number Guessing Game (GUI)/Number Guessing Game (GUI)/numpad.cs
+++ C#/Number Guessing Game (GUI)/Number Guessing Game (GUI)/numpad.cs
@@ -22,48 +22,9 @@ namespace NGG_GUI
         private void NumpadClick(object sender, EventArgs e)
         {
             Button numpadBtn = sender as Button;
-            String guess = "";
-            switch(numpadBtn.Text)
-            {
-                case "1":
-                    guess = "1";
-                    break;
-                case "2":
-                    guess = "2";
-                    break;
-                case "3":
-                    guess = "2";
-                    break;
-                case "4":
-                    guess = "4";
-                    break;
-                case "5":
-                    guess = "5";
-                    break;
-                case "6":
-                    guess = "6";
-                    break;
-                case "7":
-                    guess = "7";
-                    break;
-                case "8":
-                    guess = "8";
-                    break;
-                case "9":
-                    guess = "9";
-                    break;
-                case "0":
-                    guess = "0";
-                    break;
-                case "DEL":
-                    Gui.delChar();
-                    break;
-                case "Ka":
-                    break;
-                default:
-                    break;
-            }
-            Gui.addGuess(guess);
+            if (numpadBtn.Text.Equals("DEL")) Gui.delChar();
+            else if (numpadBtn.Text.Equals("Ka")) Gui.delChar(); // Dummy Call, will need to changed to something usefull
+            else Gui.addGuess(numpadBtn.Text);
         }
     }
 }
